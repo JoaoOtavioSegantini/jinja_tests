9679cd2ce14c860b7830be4cb5e954398ff41838
João Otávio <joaotavioos@outlook.com>
2024-02-04 13:13:09 -0300

    feat: Initial commit	🎉

---
+++ src/jinja_tests/com_pdf_tests.py
@@ -0,0 +1,48 @@
+"""Module providing a function for logs."""
+import logging
+
+from jinja2 import Environment, FileSystemLoader
+from pdfkit import from_string
+
+
+def criar_pdf():
+    """Função que popula um template através dos pacotes jinja2 e pdfkit."""
+
+    env = Environment(loader=FileSystemLoader("src/jinja_tests/templates"))
+
+    template_variaveis = {
+        'nome': 'Joao Otavio',
+        'processo': 'ddkcc 8855/2025'
+    }
+
+    template_output = env.get_template("test.html").render(template_variaveis)
+
+    conteudo_arquivo = from_string(
+        template_output,
+        False,
+        css=["src/jinja_tests/styles/template.css"]
+    )
+
+    return conteudo_arquivo
+
+
+def salvar_pdf(conteudo_pdf) -> None:
+    """Função que salva o pdf na pasta out através de um parâmetro que pode 
+    ser criado através da função criar_pdf()."""
+
+    try:
+        with open("src/jinja_tests/out/resultado.pdf", 'wb+') as pdf:
+            pdf.write(conteudo_pdf)
+    except Exception as error: #pylint: disable=W0718:broad-exception-caught
+        logging.error("Falha ao salvar o arquivo. Erro: %s", error)
+
+
+def main():
+    """Inicia a execução do processo de criação do pdf"""
+    conteudo_pdf = criar_pdf()
+    salvar_pdf(conteudo_pdf=conteudo_pdf)
+    print("Arquivo salvo com sucesso!")
+
+
+if __name__ == '__main__':
+    main()
