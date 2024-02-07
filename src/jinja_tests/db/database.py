c8321044720340a72d3381938625be6b8cc573d8
João Otávio <joaotavioos@outlook.com>
2024-02-07 06:36:53 -0300

    refactor: code refactoring 📦

--- src/jinja_tests/db/database.py
+++ src/jinja_tests/db/database.py
@@ -3,23 +3,33 @@ from dotenv import load_dotenv, get_key
 
 
 class Database:
+    """retorna uma instância do banco de dados"""
+    
+    load_dotenv()
+    
+    def __init__(self) -> None:
+        self.__server = 'sqlserver'
+        self.__database = 'FakeStore'
+        self.__username = 'sa'
+        self.__password = get_key(key_to_get='SQL_SERVER_PASS', dotenv_path='.env')
+        
 
-    def connect() -> pyodbc.Connection:
-
-        load_dotenv()
-
-        server = 'sqlserver;'
-        database = 'FakeStore'
-        username = 'sa'
-        password = get_key(key_to_get='SQL_SERVER_PASS', dotenv_path='.env')
+    def connect(self) -> pyodbc.Connection:
+        """abre a conexao com o banco de dados"""
 
         conexao = f"""
-        Driver={{ODBC Driver 18 for SQL Server}};
-        Server={server};
-        Database={database};
-        UID={username};
-        PWD={password};
-        TrustServerCertificate=yes;
-      """
+            Driver={{ODBC Driver 18 for SQL Server}};
+            Server={self.__server};
+            Database={self.__database};
+            UID={self.__username};
+            PWD={self.__password};
+            TrustServerCertificate=yes;
+        """
 
         return pyodbc.connect(conexao)
+    
+    def fechar_conexao(self, cursor: pyodbc.Cursor, conexao: pyodbc.Connection) -> None:
+        """fecha a conexao com o banco de dados"""
+
+        cursor.close()
+        conexao.close()
--- src/jinja_tests/test.py
+++ src/jinja_tests/test.py
@@ -1,11 +1,23 @@
 from db.database import Database
+from jinja2 import Environment, FileSystemLoader
 
+
+env = Environment(loader=FileSystemLoader("src/jinja_tests/templates"))
+template = env.get_template(name="processo.html.jinja")
 
 SQL = "select * from production.brands"
-conn = Database.connect()
+instance = Database()
+conn = instance.connect()
 cursor = conn.cursor()
 cursor.execute(SQL)
 
-print(cursor.fetchall())
+templ_vars = dict(brand=cursor.fetchall())
+SQL = "SELECT * FROM production.products WHERE product_id = 1"
+cursor.execute(SQL)
+dados = cursor.fetchone()
+ 
+templ_vars.update(produto=dados)
 
-cursor.close()
+print(template.render(templ_vars))
+
+instance.fechar_conexao(cursor=cursor, conexao=conn)
